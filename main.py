a96b3e3c47ce3a471045e475463125bb6dc78c64
gelno55 <glenn.de.kleyn@outlook.be>
2025-11-13 20:27:15 +0100

    added cmd parameters

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import sys
 from stats import get_num_words, get_num_char, sorted_chars
 
 def main():
-    book_path = "books/frankenstein.txt"
+    try:
+        sys.argv[1]
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_char(text)
